5d267d437f634e65adf6ef4ade317ac728dfe19d
LuigiDiS <luigi.distefano69@gmail.com>
2022-10-07 16:24:34 +0200

    problemi footer.scss

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,89 @@
+<div class="big">
+    <div class="bg-color-cyan">
+        {{-- <HeaderFooter /> --}}
+    </div>
+    <div class=bg-img-footer>
+        <div class="mid container">
+            <div class="dc-logo-big">
+                <div class="container-mid">
+                    <ul class="col">
+                        <li>
+                            <h3>Dc Comics</h3>
+                        </li>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                        <li>
+                            <h3>Shop</h3>
+                        </li>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                    <ul class="col">
+                        <li>
+                            <h3>DC</h3>
+                        </li>
+                        <li>Terms Of Use</li>
+                        <li>Privacy policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Raings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                    <ul class="col">
+                        <li>
+                            <h3>Sites</h3>
+                        </li>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-color-grey">
+        <div class="end container">
+            <div class="col button">
+                <a href="#" class="sign-up">Sign-up now!</a>
+            </div>
+            <div class="col">
+                <ul class="icons">
+                    <li>
+                        <a href="#" class="text_follow">Follow Us</a>
+                    </li>
+                    <li>
+                        <img src="../assets/img/footer-facebook.png" alt="Facebook Logo">
+                    </li>
+                    <li>
+                        <img src="../assets/img/footer-twitter.png" alt="Twitter Logo">
+                    </li>
+                    <li>
+                        <img src="../assets/img/footer-youtube.png" alt="Youtube Logo">
+                    </li>
+                    <li>
+                        <img src="../assets/img/footer-pinterest.png" alt="Pinterest Logo">
+                    </li>
+                    <li>
+                        <img src="../assets/img/footer-periscope.png" alt="Periscope Logo">
+                    </li>
+                </ul>
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
