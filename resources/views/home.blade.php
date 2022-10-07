6c41f0d77f5fde2731c2b8ed259a44f5abe0e77f
LuigiDiS <luigi.distefano69@gmail.com>
2022-10-07 15:41:37 +0200

    layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dc Comics</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('metaTitle', 'Home Page DC Comics')
+
+@section('content')
     <div>
-        Hello world!
+        <h1>Benvenuti nel Dc Comics store</h1>
     </div>
-</body>
-</html>
+
+
+@endsection
