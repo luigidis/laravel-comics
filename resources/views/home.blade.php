4ffc522896fa585d79a1002b8edbe3e5ec597599
LuigiDiS <luigi.distefano69@gmail.com>
2022-10-08 23:15:33 +0200

    done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,18 @@
 @section('metaTitle', 'Home Page DC Comics')
 
 @section('content')
-    <div>
-        <h1>Benvenuti nel Dc Comics store</h1>
+    <div class="bg-color-black">
+        <div class="container">
+            @forelse($comics as $comic)
+            <div class="card">
+                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
+                    <h3>{{ $comic['series'] }}</h3>
+            </div>
+            @empty
+                <p>Non ci sono comics disponibili!</p>
+            @endempty
+        </div>
     </div>
-@endsection
+    @endsection
+    
+
